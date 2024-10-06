de55d0b5b4eef41697239039158a4f7f899210dc
Anakor Joshua <becomingadev@gmail.com>
2024-10-06 12:16:19 +0100

    feature: create tenant dashboard mobile layout and pages

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,102 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavLink from "@/components/general/navlink";
+import {
+  LucideBellRing,
+  LucideHistory,
+  LucideHousePlus,
+  LucideLayoutDashboard,
+  LucideSettings,
+  LucideShoppingCart,
+} from "lucide-react";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      <Topbar />
+      <main className="px-5 pb-20 pt-5">{children}</main>
+      <Menu />
+    </section>
+  );
+}
+
+const Topbar = () => {
+  return (
+    <header className="flex h-16 justify-between overflow-hidden border px-5">
+      <Image
+        src="/logos/logo-transparent.png"
+        alt="Oga landlord logo"
+        width={96}
+        height={96}
+        priority
+        className="-mt-2 h-24 w-24"
+      />
+
+      <div className="flex items-center gap-x-4">
+        <Link href="">
+          <LucideBellRing />
+        </Link>
+        <Link href="">
+          <LucideShoppingCart />
+        </Link>
+        <Link href="">
+          <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <footer>
+      <ul className="fixed bottom-0 flex w-full justify-between rounded-t-3xl bg-foreground px-5 py-4 text-background">
+        <li>
+          <NavLink
+            href="/dashboard"
+            exact
+            className="flex flex-col items-center justify-center gap-y-1 text-sm"
+            activeClassName="text-primary font-semibold"
+          >
+            <LucideLayoutDashboard className="h-5 w-5" />
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            href="/dashboard/properties"
+            className="flex flex-col items-center justify-center gap-y-1 text-sm"
+            activeClassName="text-primary font-semibold"
+          >
+            <LucideHousePlus className="h-5 w-5" />
+            Properties
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            href="/dashboard/history"
+            className="flex flex-col items-center justify-center gap-y-1 text-sm"
+            activeClassName="text-primary font-semibold"
+          >
+            <LucideHistory className="h-5 w-5" />
+            History
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            href="/dashboard/settings"
+            className="flex flex-col items-center justify-center gap-y-1 text-sm"
+            activeClassName="text-primary font-semibold"
+          >
+            <LucideSettings className="h-5 w-5" />
+            Settings
+          </NavLink>
+        </li>
+      </ul>
+    </footer>
+  );
+};
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,12 @@
 export default function DashboardHomePage() {
-  return <div>page</div>;
+  return (
+    <>
+      <section>
+        <h2 className="text-3xl font-semibold">
+          <span className="block">Welcome Back,</span>
+          <span className="block">Tenant</span>
+        </h2>
+      </section>
+    </>
+  );
 }
