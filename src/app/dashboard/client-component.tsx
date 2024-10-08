85e414dfdcee2745fb73dfad57f07ed7c88338b7
Anakor Joshua <becomingadev@gmail.com>
2024-10-08 23:19:34 +0100

    install react-swipeable; create properties detail page

--- src/app/dashboard/client-component.tsx
+++ src/app/dashboard/client-component.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useState } from "react";
-import { EyeIcon, EyeOffIcon, ShieldCheckIcon } from "lucide-react";
-import { routes } from "@/constants/routes";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { routes } from "@/constants/routes";
+import { EyeIcon, EyeOffIcon, ShieldCheckIcon } from "lucide-react";
+import { useSwipeable } from "react-swipeable";
 
 export function WalletOverview() {
   const [isVisible, setVisibility] = useState(true);
@@ -33,3 +35,84 @@ export function WalletOverview() {
     </section>
   );
 }
+
+const images = [
+  "/images/duplex.webp",
+  "/images/image-2.jpeg",
+  "/images/image-3.jpeg",
+];
+
+const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1,
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
+    );
+  };
+
+  // Handle swipe gestures
+  const handlers = useSwipeable({
+    onSwipedLeft: nextSlide,
+    onSwipedRight: prevSlide,
+    preventScrollOnSwipe: true, // Prevent swiping on touch slide
+    trackMouse: true, // Enables swipe with the mouse for testing on desktops
+  });
+
+  // Auto-slide functionality (move to the next slide every 3 seconds)
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div
+        className="relative mx-auto flex h-[30vh] w-full items-center justify-center"
+        {...handlers} // Attach swipe handlers
+      >
+        <button
+          onClick={prevSlide}
+          className="absolute left-0 z-10 hidden rounded-full bg-black bg-opacity-50 p-2 text-white hover:bg-opacity-75"
+        >
+          ❮
+        </button>
+
+        <div className="relative h-full w-full overflow-hidden rounded-xl">
+          <Image
+            src={images[currentIndex]}
+            alt={`Slide ${currentIndex + 1}`}
+            fill
+            className="w-full object-cover"
+            priority
+          />
+        </div>
+
+        <button
+          onClick={nextSlide}
+          className="absolute right-0 z-10 hidden rounded-full bg-black bg-opacity-50 p-2 text-white hover:bg-opacity-75"
+        >
+          ❯
+        </button>
+      </div>
+
+      <div className="mt-2 flex justify-center space-x-2">
+        {images.map((_, i) => (
+          <button
+            key={i}
+            className={`h-1.5 rounded bg-accent ${currentIndex === i ? "w-8 bg-accent" : "w-1.5 bg-accent/40"}`}
+          ></button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ImageSlider;
--- src/app/dashboard/properties/[propertyId]/page.tsx
+++ src/app/dashboard/properties/[propertyId]/page.tsx
@@ -1,3 +1,75 @@
+import PrevPageButton from "@/components/general/prev-page";
+import { BathIcon, BedIcon, HeartIcon, MapPin, RulerIcon } from "lucide-react";
+import ImageSlider from "../../client-component";
+
 export default function PropertyDetails() {
-  return <div>property details</div>;
+  return (
+    <>
+      <section className="sticky top-0 z-50 flex w-full items-center justify-between border-b border-gray-200 bg-white px-3 py-5">
+        <div className="flex gap-x-4">
+          <PrevPageButton />
+          <h1 className="text-xl font-semibold">Emperica Dazil, Villa</h1>
+        </div>
+      </section>
+
+      <main className="p-3 pb-28">
+        <ImageSlider />
+
+        <section className="mb-4 flex items-center justify-between pt-4">
+          <div>
+            <p className="text-sm font-semibold text-accent">Apartment</p>
+            <h2 className="text-lg font-semibold">Emperica Dazil, Villa</h2>
+            <p className="flex items-center gap-x-1 text-[14px]">
+              <MapPin size={13} /> Palaxisto Emeriando Plaza Road
+            </p>
+          </div>
+          <HeartIcon />
+        </section>
+
+        <ul className="mb-6 mt-4 flex items-center justify-between rounded-lg border border-gray-200 bg-black/[1%] px-3 py-2">
+          <li className="flex items-center gap-x-1 text-[14px] font-semibold">
+            <BedIcon className="text-primary-dark" size={18} />
+            <span className="opacity-50">6 bedroom</span>
+          </li>
+          <li className="flex items-center gap-x-1 text-[14px] font-semibold">
+            <BathIcon className="text-primary-dark" size={18} />
+            <span className="opacity-50">6 bathroom</span>
+          </li>
+          <li className="flex items-center gap-x-1 text-[14px] font-semibold">
+            <RulerIcon className="text-primary-dark" size={18} />
+            <span className="opacity-50">2.62 sq ft</span>
+          </li>
+        </ul>
+
+        <div className="mb-6">
+          <h3 className="mb-1 text-[14px] leading-none opacity-50">Price</h3>
+          <p className="text-2xl font-semibold">₦10,000,000</p>
+        </div>
+
+        <section>
+          <h3 className="mb-2 text-[14px] leading-none opacity-50">
+            Description
+          </h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
+            doloremque asperiores impedit, mollitia voluptas exercitationem
+            corrupti. Enim facere quam eum? Impedit officiis porro cupiditate
+            nostrum tempore veniam earum? Doloremque quod eaque consectetur ipsa
+            rem quae perspiciatis, cupiditate necessitatibus, vero ex vel
+            accusamus veritatis deserunt atque officiis voluptates eligendi
+            ratione autem!
+          </p>
+        </section>
+      </main>
+
+      <footer className="fixed bottom-0 flex w-full items-center justify-between space-x-6 border-t border-t-gray-300 bg-white px-6 pb-8 pt-4">
+        <button className="w-full rounded-lg border-t border-gray-300 bg-foreground py-3 text-lg font-semibold text-white">
+          Get in touch
+        </button>
+        <button className="w-full rounded-lg bg-primary py-3 text-lg font-semibold text-black">
+          Add to Cart
+        </button>
+      </footer>
+    </>
+  );
 }
