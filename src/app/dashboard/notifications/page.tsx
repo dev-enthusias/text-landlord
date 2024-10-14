2bcd6230fde1ff4ffbd57dfd4ea87e21a45e3427
Anakor Joshua <becomingadev@gmail.com>
2024-10-14 12:35:17 +0100

    implement dropdown for notification on desktop screen

--- src/app/dashboard/notifications/page.tsx
+++ src/app/dashboard/notifications/page.tsx
@@ -1,5 +1,5 @@
 import PrevPageButton from "@/components/general/prev-page";
-import { MailIcon, MailOpenIcon } from "lucide-react";
+import NotificationCard from "./notification-card";
 
 export default function Notifications() {
   return (
@@ -31,31 +31,5 @@ export default function Notifications() {
         </div>
       </section>
     </>
-  );
-}
-
-function NotificationCard({
-  status = "unread",
-}: {
-  status?: "unread" | "read";
-}) {
-  return (
-    <article className="border-b-grey-200 flex gap-x-2 border-b bg-white px-3 py-5">
-      <div className="relative flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-primary/10 text-primary-dark">
-        {status === "read" ? <MailOpenIcon /> : <MailIcon />}
-        {status === "unread" && (
-          <div className="absolute left-2.5 top-3 h-2 w-2 rounded-full bg-accent" />
-        )}
-      </div>
-      <div>
-        <h3 className="font-semibold">Report successfully created</h3>
-        <p className="w-[calc(100vw-82px)] truncate text-sm opacity-80">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
-          reiciendis fugit odit autem qui ipsum itaque dolore eum? Rem,
-          possimus!
-        </p>
-        <p className="text-sm font-medium opacity-40">1h ago</p>
-      </div>
-    </article>
   );
 }
--- src/components/general/notification-btn.tsx
+++ src/components/general/notification-btn.tsx
@@ -1,38 +1,40 @@
-// "use client";
+"use client";
 
-// // import { NotificationCard } from "@/app/dashboard/notifications/page";
-// import { LucideBellRing } from "lucide-react";
-// import { useState } from "react";
+import NotificationCard from "@/app/dashboard/notifications/notification-card";
+import { LucideBellRing } from "lucide-react";
+import { useState } from "react";
 
-// export default function NotificationBtn() {
-//   const [isOpen, setIsOpen] = useState(false);
+export default function NotificationBtn() {
+  const [isOpen, setIsOpen] = useState(false);
 
-//   return (
-//     <div className="relative">
-//       <button className="hidden lg:block" onClick={() => setIsOpen(!isOpen)}>
-//         <LucideBellRing />
-//       </button>
+  return (
+    <div>
+      <button className="hidden lg:block" onClick={() => setIsOpen(!isOpen)}>
+        <LucideBellRing />
+      </button>
 
-//       {isOpen && (
-//         <div className="absolute right-0 top-20 z-[1000000000] w-[420px]">
-//           <section className="flex items-center justify-between border-b border-gray-200 bg-white px-3 py-5">
-//             <div className="flex gap-x-4">
-//               <h1 className="text-xl font-semibold">Notifications</h1>
-//             </div>
-//             <button className="text-[14px] font-semibold text-accent">
-//               Mark all as read
-//             </button>
-//           </section>
-//           <section>
-//             <div>
-//               {/* <NotificationCard />
-//               <NotificationCard />
-//               <NotificationCard status="read" />
-//               <NotificationCard status="read" /> */}
-//             </div>
-//           </section>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+      {isOpen && (
+        <div className="custom-shadow fixed right-0 top-20 flex h-[calc(100vh-80px)] w-[420px] flex-col bg-white">
+          <section className="flex items-center justify-between border-b border-gray-200 bg-white px-3 py-5">
+            <div className="flex gap-x-4">
+              <h1 className="text-xl font-semibold">Notifications</h1>
+            </div>
+            <button className="text-[14px] font-semibold text-accent">
+              Mark all as read
+            </button>
+          </section>
+          <section className="no-scrollbar h-full grow overflow-y-scroll">
+            <div>
+              <NotificationCard />
+              <NotificationCard />
+              <NotificationCard status="read" />
+              <NotificationCard status="read" />
+              <NotificationCard status="read" />
+              <NotificationCard status="read" />
+            </div>
+          </section>
+        </div>
+      )}
+    </div>
+  );
+}
